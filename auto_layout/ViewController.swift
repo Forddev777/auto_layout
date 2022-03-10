bdd6310fc7d30953676ed4b4fa0a3887027643fe
Forddev777 <taothongdev@gmail.com>
2022-03-10 21:16:23 +0700

    Initial Commit

---
+++ auto_layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  auto_layout
+//
+//  Created by suriya taothongkom on 10/3/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
